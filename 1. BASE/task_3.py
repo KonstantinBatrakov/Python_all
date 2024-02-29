5d90d3e6b514ca793d4fb57769f48447a137a8fe
Batrakov Konstantin <118470335+KonstantinBatrakov@users.noreply.github.com>
2024-02-29 11:06:15 +0100

    Add files via upload

---
+++ 1. BASE/task_3.py
@@ -0,0 +1,31 @@
+"""
+3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов.
+Попробуйте решить задачу двумя способами:
+1) используя функцию sort()
+2) без функции sort()
+"""
+
+
+# Вариант 1
+def get_max(*args):
+    print(sum(sorted(list(args), reverse=True)[:2]))
+
+
+get_max(100, 1, 200)
+
+
+# Вариант 2
+def get_max(*args):
+    lst = list(args)
+    i = 0
+    res = 0
+    while i != 2:
+        max_val = max(lst)
+        res += max_val
+        lst.remove(max_val)
+        i += 1
+    print(res)
+
+
+get_max(100, 1, 200)
